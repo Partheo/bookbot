205989fe8e92e760e887761c1193f3be1487c3e9
Partheo <therarpar@gmail.com>
2025-03-25 20:13:18 -0400

    add report and counting functionality

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from stats import count_words
+from stats import count_characters
+from stats import sort_by_occurences
 
 BOOK_TO_READ = "./books/frankenstein.txt"
 
 def main():
-    num_words = count_words(get_book_text(BOOK_TO_READ))
-    print(f"{num_words} words found in the document")
+    book_text = get_book_text(BOOK_TO_READ)
+
+    num_words = count_words(book_text)
+    num_characters = count_characters(book_text)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for pair in sort_by_occurences(num_characters):
+        if pair["character"].isalpha():
+            print(f"{pair["character"]}: {pair["count"]}")
+
+    print("============= END ===============")
 
 def get_book_text(path):
     text = ""
--- stats.py
+++ stats.py
@@ -1,2 +1,25 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    character_occurences = {}
+    for char in text.lower():
+        if char not in character_occurences:
+            character_occurences[char] = 1
+            continue
+        character_occurences[char] += 1
+
+    return character_occurences
+
+def sort_by_occurences(dict):
+    character_counts = []
+    for char in dict:
+        pair = {"character": char, "count": dict[char]}
+        character_counts.append(pair)
+
+    character_counts.sort(reverse=True, key=sort_on)
+
+    return character_counts
+
+def sort_on(dict):
+    return dict["count"]
