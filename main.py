71bfc06e99716cbd3101ce8e0f943353f241b3c7
Partheo <therarpar@gmail.com>
2025-03-25 21:37:02 -0400

    add support for file argument

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import sort_by_occurences
 
-BOOK_TO_READ = "./books/frankenstein.txt"
-
 def main():
-    book_text = get_book_text(BOOK_TO_READ)
+    if not len(sys.argv) == 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_book = sys.argv[1]
+
+    book_text = get_book_text("./" + path_to_book)
 
     num_words = count_words(book_text)
     num_characters = count_characters(book_text)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
